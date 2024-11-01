7f7c508c69caf64a4af2f4737381e16c6ee6a738
sashkashishka <alexandr1696@hotmail.com>
2024-11-01 20:17:04 +0100

    feat: init db layout

--- server/src/db/migrations/0000_busy_triathlon.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `admin` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`password` text NOT NULL
-);
--- server/src/db/schema.js
+++ server/src/db/schema.js
@@ -5,3 +5,38 @@ export const adminTable = sqliteTable('admin', {
   name: text().notNull(),
   password: text().notNull(),
 });
+
+export const userTable = sqliteTable('user', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  surname: text().notNull(),
+});
+
+export const programTable = sqliteTable('program', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  userId: integer('user_id').references(() => userTable.id),
+});
+
+export const taskTable = sqliteTable('task', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  type: text({ enum: ['slider'] }).notNull(),
+  config: text({ mode: 'json' }),
+});
+
+export const labelTable = sqliteTable('label', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+});
+
+export const programTaskTable = sqliteTable('program_task', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  programId: integer('program_id').references(() => programTable.id),
+  taskId: integer('task_id').references(() => taskTable.id),
+});
+
+export const taskLabelTable = sqliteTable('task_label', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  labelId: integer('label_id').references(() => labelTable.id),
+  taskId: integer('task_id').references(() => taskTable.id),
+});
