93c5e74178e1f27a22a71ce65e4977784c469c6e
sashkashishka <alexandr1696@hotmail.com>
2024-11-06 16:29:32 +0100

    fix: typings

--- server/src/core/composer.d.ts
+++ server/src/core/composer.d.ts
@@ -10,10 +10,10 @@ export type Middleware<Ctx> = MiddlewareFn<Ctx> | MiddlewareObj<Ctx>;
 
 export class Composer<Ctx> {
   use(...fns: Middleware<Ctx>[]): this;
-  get(...fns: Middleware<Ctx>[]): this;
-  put(...fns: Middleware<Ctx>[]): this;
-  post(...fns: Middleware<Ctx>[]): this;
-  patch(...fns: Middleware<Ctx>[]): this;
-  delete(...fns: Middleware<Ctx>[]): this;
+  get(route: string, ...fns: Middleware<Ctx>[]): this;
+  put(route: string, ...fns: Middleware<Ctx>[]): this;
+  post(route: string, ...fns: Middleware<Ctx>[]): this;
+  patch(route: string, ...fns: Middleware<Ctx>[]): this;
+  delete(route: string, ...fns: Middleware<Ctx>[]): this;
   middleware: MiddlewareFn<Ctx>;
 }
