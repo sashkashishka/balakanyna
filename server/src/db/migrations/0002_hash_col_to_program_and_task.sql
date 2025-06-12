149f14722387c2d2ed5918a406c2166aa0fb0eaa
sashkashishka <alexandr1696@hotmail.com>
2025-06-12 19:33:12 +0200

    feat: proper migration

--- server/src/db/migrations/0002_hash_col_to_program_and_task.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `program` ADD `hash` text DEFAULT '';
-ALTER TABLE `task` ADD `hash` text DEFAULT '';
-
-UPDATE `program` SET hash = id;
-UPDATE `task` SET hash = id;
---
+++ server/src/db/migrations/0002_reflective_mentor.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `program` ADD `hash` text DEFAULT '';--> statement-breakpoint
+ALTER TABLE `task` ADD `hash` text DEFAULT '';
