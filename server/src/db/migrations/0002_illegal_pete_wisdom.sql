e645d85864da091b2cfb19518aa78042164c0f1d
sashkashishka <alexandr1696@hotmail.com>
2025-06-12 17:17:04 +0200

    Revert "feat: add hash field to program and task tables"
    This reverts commit 5075e13cf8c6d5b90158cb3453b9bdefd04db0d2.

--- server/src/db/migrations/0002_illegal_pete_wisdom.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `program` ADD `hash` text NOT NULL;--> statement-breakpoint
-ALTER TABLE `task` ADD `hash` text NOT NULL;
--- server/src/db/schema.js
+++ server/src/db/schema.js
@@ -29,7 +29,6 @@ export const userTable = sqliteTable('user', {
 
 export const programTable = sqliteTable('program', {
   id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-  hash: text().notNull(),
   name: text().notNull(),
   userId: integer('user_id').references(() => userTable.id),
   startDatetime: text().default(sql`(datetime('now'))`),
@@ -40,7 +39,6 @@ export const programTable = sqliteTable('program', {
 
 export const taskTable = sqliteTable('task', {
   id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-  hash: text().notNull(),
   name: text().notNull(),
   type: text({ enum: tasks }).notNull(),
   config: text({ mode: 'json' }),
