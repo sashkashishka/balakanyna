eccf2308da47a7a41efc7b41516698ad77d4b3ff
sashkashishka <alexandr1696@hotmail.com>
2025-06-13 21:16:41 +0200

    feat: findFlashingNumber api

---
+++ server/src/test/middleware/api/admin/task/create/create-findFlashingNumber.test.js
@@ -0,0 +1,138 @@
+import { describe, test } from 'node:test';
+import assert from 'node:assert/strict';
+
+import { getTestServer } from '../../../../../helpers/getTestServer.js';
+import { getAuthCookie } from '../../../../../helpers/utils.js';
+
+import * as taskCreate from '../../../../../../middleware/api/admin/task/create/middleware.js';
+
+import { seedAdmins } from '../../../../../../db/seeders.js';
+import { admin } from '../../fixtures/admin.js';
+import { findFlashingNumberTask } from '../../fixtures/task.js';
+
+describe('[api] task create findFlashingNumber', () => {
+  test('should retun 400 if config is invalid', async (t) => {
+    const { request } = await getTestServer({
+      t,
+      async seed(db, config) {
+        await seedAdmins(db, [admin], config.salt.password);
+      },
+    });
+
+    const payload = {
+      ...findFlashingNumberTask,
+      config: {
+        foo: 1,
+      },
+    };
+
+    const resp = await request(taskCreate.route, {
+      method: taskCreate.method,
+      headers: {
+        cookie: await getAuthCookie(request, admin),
+      },
+      body: payload,
+    });
+    const body = await resp.json();
+
+    assert.equal(resp.status, 400);
+    assert.deepEqual(body, {
+      error: 'INVALID_TASK_CONFIG',
+      message: `Invalid task config for ${payload.type}`,
+    });
+  });
+
+  test('should return 400 if positionalDigints are not withing the range', async (t) => {
+    const { request } = await getTestServer({
+      t,
+      async seed(db, config) {
+        await seedAdmins(db, [admin], config.salt.password);
+      },
+    });
+
+    const payload = {
+      ...findFlashingNumberTask,
+      config: {
+        ...findFlashingNumberTask.config,
+        positionalDigit: { min: 1, max: 5 },
+      },
+    };
+
+    const resp = await request(taskCreate.route, {
+      method: taskCreate.method,
+      headers: {
+        cookie: await getAuthCookie(request, admin),
+      },
+      body: payload,
+    });
+    const body = await resp.json();
+
+    assert.equal(resp.status, 400);
+    assert.deepEqual(body, {
+      error: 'INVALID_TASK_CONFIG',
+      message: `Invalid task config for ${payload.type}`,
+    });
+  });
+
+  test('should return 400 if positionalDigints are not withing the range 2', async (t) => {
+    const { request } = await getTestServer({
+      t,
+      async seed(db, config) {
+        await seedAdmins(db, [admin], config.salt.password);
+      },
+    });
+
+    const payload = {
+      ...findFlashingNumberTask,
+      config: {
+        ...findFlashingNumberTask.config,
+        positionalDigit: { min: 5, max: 1 },
+      },
+    };
+
+    const resp = await request(taskCreate.route, {
+      method: taskCreate.method,
+      headers: {
+        cookie: await getAuthCookie(request, admin),
+      },
+      body: payload,
+    });
+    const body = await resp.json();
+
+    assert.equal(resp.status, 400);
+    assert.deepEqual(body, {
+      error: 'INVALID_TASK_CONFIG',
+      message: `Invalid task config for ${payload.type}`,
+    });
+  });
+
+  test('should return 200 and create task', async (t) => {
+    const { request } = await getTestServer({
+      t,
+      async seed(db, config) {
+        await seedAdmins(db, [admin], config.salt.password);
+      },
+    });
+
+    const payload = findFlashingNumberTask;
+
+    const resp = await request(taskCreate.route, {
+      method: taskCreate.method,
+      headers: {
+        cookie: await getAuthCookie(request, admin),
+      },
+      body: payload,
+    });
+    const body = await resp.json();
+
+    assert.equal(resp.status, 200);
+    assert.equal(typeof body.id, 'number');
+    assert.equal(body.hash.length, 8);
+    assert.equal(body.name, payload.name);
+    assert.equal(body.type, payload.type);
+    assert.deepEqual(body.config.list, payload.config.list);
+    assert.equal(isNaN(new Date(body.createdAt)), false);
+    assert.equal(isNaN(new Date(body.updatedAt)), false);
+    assert.equal(Object.keys(body).length, 7);
+  });
+});
