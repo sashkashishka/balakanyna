bec6ef81a2a62ed70ebee1f88e26220e79d76c81
sashkashishka <alexandr1696@hotmail.com>
2025-06-18 00:22:53 +0200

    feat: add bgs for default preset

--- client/src/tasks/GoneAndFound/constants.ts
+++ client/src/tasks/GoneAndFound/constants.ts
@@ -1,4 +1,4 @@
-import type { TItemPresets } from './types.ts';
+import type { TItemPreset, TItemPresets } from './types.ts';
 
 import avocado from './images/avocado.png';
 import broccoli from './images/broccoli.png';
@@ -12,6 +12,9 @@ import lemon from './images/lemon.png';
 import onion from './images/onion.png';
 import tomato from './images/tomato.png';
 import zucchini from './images/zucchini.png';
+
+import defaultFieldBg from './images/grass.webp';
+import defaultPickBg from './images/wood-crate.png';
 
 export const ITEM_PRESETS: TItemPresets = {
   default: [
@@ -28,6 +31,16 @@ export const ITEM_PRESETS: TItemPresets = {
     tomato,
     zucchini,
   ],
+};
+
+export const ITEM_PRESETS_BG: Record<
+  TItemPreset,
+  { fieldBg: string; pickBg: string }
+> = {
+  default: {
+    fieldBg: defaultFieldBg,
+    pickBg: defaultPickBg,
+  },
 };
 
 export const ITEMS_LENGTH = 12;
